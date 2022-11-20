e2c1bd6310b988f23b4f6d5aa44c6342f1983407
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-21 00:40:01 +0530

    Added Power of 2 solution

--- JavaBasicDay_5/src/main/java/org/example/Main.java
+++ JavaBasicDay_5/src/main/java/org/example/Main.java
@@ -4,24 +4,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Main obj = new Main();
-       // 1. Flip Coin and print percentage of Heads and Tails
-        obj.flipCoin();
-        System.out.println("---------------------------------");
-        //2. Leap Year
+        System.out.println("Enter Problem Number : ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter year to check leap year or not : ");
-        int year=sc.nextInt();
-        obj.leapYear(year);
-        System.out.println("---------------------------------");
+        int probNumber = sc.nextInt();
+        Main obj = new Main();
+        switch (probNumber) {
+            case 1:
+                obj.flipCoin();
+                break;
+            case 2:
+                obj.leapYear();
+                break;
+            case 3:
+                obj.powerOfTwo();
+                break;
+        }
     }
 
-    private void leapYear(int year) {
+    private void powerOfTwo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int number = sc.nextInt();
+        for (int i = 0; i <= number; i++) {
+            int table = (int) Math.pow(2, i);
+            System.out.println(table);
+        }
+    }
 
-        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-            System.out.println(year+" is a leap year");
-        else
-            System.out.println(year+" is not a leap year");
+    private void leapYear() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year to check leap year or not : ");
+        int year = sc.nextInt();
+        int digit = (int) Math.floor(Math.log10(year) + 1);
+        if (digit == 4) {
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                System.out.println(year + " is a leap year");
+            else
+                System.out.println(year + " is not a leap year");
+        } else System.out.println("invalid year");
     }
 
     private void flipCoin() {
@@ -35,10 +55,10 @@ public class Main {
             if (value == 0) tail++;
             else head++;
         }
-        System.out.println("head = "+head+"  Tails = "+tail);
+        System.out.println("head = " + head + "  Tails = " + tail);
         double headPercentage = head * 100 / flip;
         double tailPercentage = tail * 100 / flip;
-        System.out.println("head percentage:"+headPercentage);
-        System.out.println("tails percentage:"+tailPercentage);
+        System.out.println("head percentage:" + headPercentage);
+        System.out.println("tails percentage:" + tailPercentage);
     }
 }
